6938241979fd8c5febeb3f77d4e1d27cf806f7f4
TienTien <tiendtc2@fpt.com>
2026-01-15 00:34:12 +0700

    Create test_draganddrop.py
    hw 15/1

---
+++ practice/test_draganddrop.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+from selenium import webdriver
+import pytest
+
+
+class TestDragAndDrop:
+    def test_drag_and_drop(self, timeout=10):
+        url = "https://demo.guru99.com/test/drag_drop.html"
+        driver = webdriver.Chrome()
+        driver.get(url)
+        driver.maximize_window()
+        # driver.switch_to.frame(driver.find_element(By.CLASS_NAME, "demo-frame"))
+        wait = WebDriverWait(driver, 10)
+        drag = driver.find_element(By.ID, "fourth")
+        drop = driver.find_element(By.ID, "shoppingCart4")
+        
+        actions = ActionChains(driver)
+        actions.drag_and_drop(drag, drop).perform()
+        
+        sleep(10)  # Just to visually confirm the action during testing
+
+        # assert drop.text == "Dropped!"
