f4603f529c8c165cd88719ccd00494758db95891
tien-tien1 <tiendtc2@fpt.com>
2026-01-20 20:52:33 +0700

    update2

--- tests/test_login.py
+++ tests/test_login.py
@@ -10,17 +10,16 @@ import tests.conftest as conftest
 
 class TestLogin:
     @pytest.mark.smoke
-    def test_login(self,driver: WebDriver):
+    def test_login(self,driver):
         login_page = LoginPage(driver)
         # login_page.do_login('Admin' , 'admin123')
         login_page.do_login(ConfigReader.get_username(), ConfigReader.get_password())
         sleep(5)
         assert driver.find_element(By.XPATH, "//h6").text == "Dashboard" 
-          
-class TestLoginFail:     
-    def test_login_fail_wrong_password(self,driver: WebDriver):
+           
+    def test_login_fail_wrong_password(self,driver):
         login_page = LoginPage(driver)
-        login_page.enter_username("")
+        login_page.enter_username("a")
         login_page.enter_password(ConfigReader.get_password())
         login_page.do_login('',ConfigReader.get_password())
         sleep(5)
@@ -50,13 +49,6 @@ class TestLoginFail:
         sleep(5)
         assert driver.find_element(By.XPATH, "//p[@class='oxd-text oxd-text--p oxd-alert-content-text']").text == "Invalid credentials"
         
-# def do_login(username, password):
-#     driver.find_element(self.username).send_keys(username)
-#     driver.find_element(self.password).send_keys(password)
-#     driver.find_element(self.Click_login_btn).click()
-    # def test_login_pass(self, driver):
-    #     login_page = LoginPage(driver)
-    #     login_page.enter_username("Admin")
-    #     login_page.enter_password("admin123")
-    #     login_page.Click_login_button()
-    # sleep(5)
+
+
+    
